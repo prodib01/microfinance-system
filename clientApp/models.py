40dbb5d81d7b32301104c07ed54f588d8476fe76
Lutaaya Jamil <jamillutaaya0@gmail.com>
2024-10-06 23:12:12 +0300

    Refactor loan reports view to add "land_mark" field in Person model

--- clientApp/models.py
+++ clientApp/models.py
@@ -20,6 +20,7 @@ class Person(models.Model):
     client_code = models.CharField(max_length=20, null=True, blank=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    land_mark = models.CharField(max_length=256, null=True, blank=True, default=None)
 
     def __str__(self):
         return self.full_name
